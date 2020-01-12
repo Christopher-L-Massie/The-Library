f97360f21d272ad03130c24aab37ddb9032f8aea
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-01-12 04:50:41 -0600

    Sketched out a basic Guild Class. Will need more things added once more Classes are Sketched out.

---
+++ Realm-Library/src/main/java/com/example/RealmLibrary/models/Guild.java
@@ -0,0 +1,25 @@
+package com.example.RealmLibrary.models;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Guild {
+
+
+    @GeneratedValue
+    private int id;
+
+    private String guildName;
+
+    private int guildSize;
+
+    private String guildHistory;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Character> guildMembers = new ArrayList<>();
+}
