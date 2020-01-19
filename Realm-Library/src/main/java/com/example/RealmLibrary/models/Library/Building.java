6b0fb9983b10dd975643584b7b70558ef927620a
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-01-19 09:20:16 -0600

    Added Building Class, Restructuring the way the stories will be stored to make it more interactive.

---
+++ Realm-Library/src/main/java/com/example/RealmLibrary/models/Library/Building.java
@@ -0,0 +1,23 @@
+package com.example.RealmLibrary.models.Library;
+
+import javax.persistence.*;
+
+@Entity
+public class Building {
+
+    //||PROPERTIES||
+
+    @Id
+    @GeneratedValue
+    int id;
+
+    private String libraryName;
+
+    /*
+    witty comment
+    */
+
+    
+
+
+}
