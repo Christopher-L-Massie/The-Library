b9d0151a9e8c2818254fe95cc0f6a36f8c96464e
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-01-21 11:56:17 -0600

    Created outline of Isle Class, almost finished with the outline of needed Classes now. Will create a few more and take a break to make sure I haven't missed any before I start linking them together.

---
+++ Realm-Library/src/main/java/com/example/RealmLibrary/models/Library/Isle.java
@@ -0,0 +1,28 @@
+package com.example.RealmLibrary.models.Library;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import java.sql.Timestamp;
+
+@Entity
+public class Isle {
+
+    @Id
+    @GeneratedValue
+    int id;
+
+    private int isleNumber;
+
+    @Max(4)
+    private int isleQuadrant;
+
+    private Timestamp timeCreated = new Timestamp(System.currentTimeMillis());
+
+    //|||CONSTRUCTORS|||
+    
+    //empty for database setup
+    public Isle(){}
+
+}
