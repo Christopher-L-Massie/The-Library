5777da5e517e5870f2a70079fdf494d63f704800
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-01-22 16:34:28 -0600

    Added Accessors to Isle.java, some may be set to private later.

--- Realm-Library/src/main/java/com/example/RealmLibrary/models/Library/Isle.java
+++ Realm-Library/src/main/java/com/example/RealmLibrary/models/Library/Isle.java
@@ -28,4 +28,37 @@ public class Isle {
     //empty for database setup
     public Isle(){}
 
+    //|||ACCESSORS|||
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIsleNumber() {
+        return isleNumber;
+    }
+
+    public void setIsleNumber(int isleNumber) {
+        this.isleNumber = isleNumber;
+    }
+
+    public Timestamp getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(Timestamp timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+
+    public List<Bookcase> getBookcases() {
+        return bookcases;
+    }
+
+    public void setBookcases(List<Bookcase> bookcases) {
+        this.bookcases = bookcases;
+    }
 }
