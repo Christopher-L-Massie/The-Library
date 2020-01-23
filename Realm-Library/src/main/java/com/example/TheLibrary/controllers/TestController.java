8d5eb01ce3b729342d1833909729d7c2982780c1
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-01-23 13:24:39 -0600

    WORKING BUILD: Changes in the TestController demonstrated that everything is rigged correctly thus far. If there was an error in the database I believe it would cause a crash at this point. Will begin building simple ways to test and build the program before building a final design.

--- Realm-Library/src/main/java/com/example/TheLibrary/controllers/TestController.java
+++ Realm-Library/src/main/java/com/example/TheLibrary/controllers/TestController.java
@@ -1,6 +1,8 @@
 package com.example.TheLibrary.controllers;
 
 
+import com.example.TheLibrary.models.data.BuildingDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
@@ -9,10 +11,15 @@ import org.springframework.ui.Model;
 @RequestMapping(value = "test")
 public class TestController {
 
+    @Autowired
+    public BuildingDao buildingDao;
+
     @RequestMapping(value = "character_creation")
     public String testCharacterCreator(Model model){
 
-        return"home/index";
+        System.out.println("hello");
+
+        return"Test/index";
     }
 
 
