f0744f93275147751184a14e667ba4dcf385537c
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-10 14:17:01 -0600

    :fire:Deleted BuildingDao, it's really not needed at all the books will be kept together eitherway by the playerDao, glad I took a few days away from this project. It's much simpler than I was originally making it honestly.

--- Realm-Library/src/main/java/com/example/TheLibrary/models/data/BuildingDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.TheLibrary.models.data;
-
-import com.example.TheLibrary.models.Library.Building;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BuildingDao extends CrudRepository<Building, Integer> {
-}
