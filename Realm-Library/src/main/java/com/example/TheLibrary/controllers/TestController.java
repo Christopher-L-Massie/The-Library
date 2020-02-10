a4888ffd4edd10182e261dd2d7c9ce9c4823cd30
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-10 14:17:27 -0600

    :fire:Deleted outdated reference to BuildingDao in testController.

--- Realm-Library/src/main/java/com/example/TheLibrary/controllers/TestController.java
+++ Realm-Library/src/main/java/com/example/TheLibrary/controllers/TestController.java
@@ -1,7 +1,6 @@
 package com.example.TheLibrary.controllers;
 
 
-import com.example.TheLibrary.models.data.BuildingDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +9,6 @@ import org.springframework.ui.Model;
 @Controller
 @RequestMapping(value = "test")
 public class TestController {
-
-    @Autowired
-    public BuildingDao buildingDao;
 
     @RequestMapping(value = "book")
     public String testCharacterCreator(Model model){
