89a5e2d4e16b9aea228fc9f506431f3996ff981b
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-10 14:22:46 -0600

    :hammer:Autowired the BookDao into the TestController.

--- Realm-Library/src/main/java/com/example/TheLibrary/controllers/TestController.java
+++ Realm-Library/src/main/java/com/example/TheLibrary/controllers/TestController.java
@@ -1,6 +1,7 @@
 package com.example.TheLibrary.controllers;
 
 
+import com.example.TheLibrary.models.data.BookDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,9 @@ import org.springframework.ui.Model;
 @Controller
 @RequestMapping(value = "test")
 public class TestController {
+
+    @Autowired
+    public BookDao bookDao;
 
     @RequestMapping(value = "book")
     public String testCharacterCreator(Model model){
