0b0236c87bf90a37b0c2c09187c3225f842aca8f
Chris-L-Massie <50225179+DonFranquito@users.noreply.github.com>
2020-02-29 22:48:07 -0600

    :hammer:Added a List<Book> to User class incase I decide to require user accounts. This will keep track of the books they have written. This will also make editing books much easier and give me more practice with creating user accounts.

--- Realm-Library/src/main/java/com/example/TheLibrary/models/Accounts/User.java
+++ Realm-Library/src/main/java/com/example/TheLibrary/models/Accounts/User.java
@@ -1,9 +1,14 @@
 package com.example.TheLibrary.models.Accounts;
 
+import com.example.TheLibrary.models.Library.Book;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class User {
@@ -19,6 +24,9 @@ public class User {
     private String password;
 
     private Timestamp timeCreated = new Timestamp(System.currentTimeMillis());
+
+    @OneToMany
+    private List<Book> booksWritten = new ArrayList<>();
 
     //|||CONSTRUCTORS|||
 
@@ -37,5 +45,7 @@ public class User {
     public String getUserName() {
         return userName;
     }
-    
+
+    public List<Book> getBooksWritten() { return booksWritten;}
+
 }
